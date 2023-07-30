043e5d82dc499cd7a69af3dbbb8853696e47f358
AnnaPeBraz <annap.0103@gmail.com>
2023-07-29 22:27:26 -0300

    arrumando a navbar (incompleto)

---
+++ desafiofsfe/src/components/container.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./styles/customClass.module.css"
+
+function Container (props){
+    return (
+        <div className={'${style.container} ${style[props.customClass]'}>
+            {props.children}
+        </div>
+    )
+}
+
+export default Container;
--- desafiofsfe/src/components/navbar.jsx
+++ desafiofsfe/src/components/navbar.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import style from "./styles/navbar.module.css";
+import Container from "./container";
 
 function NavBar () {
     return(
-        <div>
+        <div className={style.navbar_container}>
             <nav>
-                <Link to="/"> Home </Link>
-                <Link to="/cadastrarEmpresa"> Empresa </Link>
+                <Container>
+                    <Link to="/" className={style.list}> Home </Link>
+                    <Link to="/cadastrarEmpresa" className={style.list}> Empresa </Link>
+                </Container>
             </nav>
         </div>
     )
